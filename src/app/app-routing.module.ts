7309c5d234d6c2b48eab5ad778c7c8e44cfe8d72
ElianaValderrama <ecvalderrama@gmail.com>
2021-11-30 20:14:20 -0500

    commit front rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TablaUsuariosComponent } from './components/tabla-usuarios/tabla-usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'usuarios', component: TablaUsuariosComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
